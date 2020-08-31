cf9932b88e1bc16319732d6569e83e8b9da90bc5
bright-lifecycle-9875 <mairgup@icloud.com>
2020-08-31 20:44:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,41 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
+  [1,2,3,4]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  array = ["wow", "I", "am", "really", "learning"]
+  array << "arrays!"
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  arrays = array.pop
 end
 
 def remove_element_from_start_of_array(array)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  w_o_w = array.shift
+  
+  
   
 end
 
 def retrieve_element_from_index(array, index_number)
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array[index_number]
+  
   
 end
 
